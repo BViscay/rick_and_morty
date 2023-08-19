db7f5a9784f5727115272658c8baaf21bb5c47f9
Bruno Viscay <126488071+BViscay@users.noreply.github.com>
2023-08-19 13:40:22 -0300

    pagination

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,13 +1,30 @@
 import Card from "../Card/Card";
 import styles from "./Cards.module.css";
 import Home from "../../img/Home.png";
+import { useState } from "react";
+
 export default function Cards(props) {
   const characters = props.characters;
+  const cardsInPage = 5;
+  const [actualPage, setActualPage] = useState(1);
+
+  const indexOfLastCard = actualPage * cardsInPage;
+  const indexOfFirstCard = indexOfLastCard - cardsInPage;
+  const currentCards = characters.slice(indexOfFirstCard, indexOfLastCard);
+
+  const nextPage = () => {
+    setActualPage(actualPage + 1);
+  };
+
+  const prevPage = () => {
+    setActualPage(actualPage - 1);
+  };
+
   return (
     <div className={styles.divCont}>
       <img className={styles.homeImg} src={Home} alt="Home" />
       <div className={styles.divCards}>
-        {characters.map((character) => (
+        {currentCards.map((character) => (
           <Card
             key={character.id}
             id={character.id}
@@ -20,6 +37,18 @@ export default function Cards(props) {
             onClose={() => props.onClose(character.id)}
           />
         ))}
+      </div>
+      <div className={styles.divPagination}>
+        {actualPage > 1 && (
+          <button onClick={prevPage} className={styles.button}>
+            Previous
+          </button>
+        )}
+        {indexOfLastCard < characters.length && (
+          <button onClick={nextPage} className={styles.button}>
+            Next
+          </button>
+        )}
       </div>
     </div>
   );
